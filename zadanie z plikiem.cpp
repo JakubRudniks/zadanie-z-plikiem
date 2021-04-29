84230f2bcb9a36c944d70528d0c7a034216d8e9b
JakubRudniks <62304977+JakubRudniks@users.noreply.github.com>
2021-04-29 12:04:19 +0200

    Add files via upload

---
+++ zadanie z plikiem.cpp
@@ -0,0 +1,40 @@
+﻿//
+// pch.cpp
+//
+
+#include "pch.h"
+#include <collection.h>
+#include <ppltasks.h>
+#include "App.xaml.h"
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    plik.open("D:\tekstowe\plik.txt");
+    while (!plik.eof())
+    {
+        int i, n, liczba, min = 1, max = 2;
+
+        cout << "Podaj ilosc liczb" << endl;
+        cin >> n;
+
+        for (i = 1; i <= n; i++)
+        {
+            cout << "Podaj liczbe" << endl;
+            cin >> liczba;
+        }
+
+        for (i = 1; i < n; i++)
+        {
+            if (liczba > max)
+            {
+                max = liczba;
+            }
+        }
+
+        cout << endl;
+        cout << "Liczba max: " << max << endl;
+
+        return 0;
+    }
